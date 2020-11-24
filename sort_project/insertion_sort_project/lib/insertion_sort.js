4384eaa3b996f0017360359d70a453b896b66f3d
nateoh93 <nathanoh93@gmail.com>
2020-11-24 10:30:23 -0800

    insertion sort

---
+++ sort_project/insertion_sort_project/lib/insertion_sort.js
@@ -0,0 +1,14 @@
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let currentEle = arr[i];
+        for (var j = i - 1; j >= 0 && currentEle < arr[j]; j--) {
+            arr[j+1] = arr[j];
+        }
+        arr[j+1] = currentEle
+    }
+    return arr
+}
+
+module.exports = {
+    insertionSort
+};
